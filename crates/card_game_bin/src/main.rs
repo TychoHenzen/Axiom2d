0aa6366f3fd927f6615566fbd313c4a139f57202
Tycho <tychohenzen@gmail.com>
2026-09-25 05:24:22 +0200

    style(card-game): format ui-test setup

--- crates/card_game_bin/src/main.rs
+++ crates/card_game_bin/src/main.rs
@@ -138,11 +138,10 @@ fn setup(app: &mut App) {
 
     #[cfg(feature = "ui-test")]
     {
-        let state_file = std::env::var_os("AXIOM_UI_TEST_STATE_FILE")
-            .map_or_else(
-                || panic!("AXIOM_UI_TEST_STATE_FILE must be set with ui-test"),
-                PathBuf::from,
-            );
+        let state_file = std::env::var_os("AXIOM_UI_TEST_STATE_FILE").map_or_else(
+            || panic!("AXIOM_UI_TEST_STATE_FILE must be set with ui-test"),
+            PathBuf::from,
+        );
         UI_TEST_STATE_FILE
             .set(state_file)
             .expect("UI test state file can only be configured once");
